4f7d53ef4200ff46806cc40490d1af75fc60d7ed
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-02-18 17:50:35 +0100

    Install React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    ),
+  },
+]);
+
 const App = (): JSX.Element => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <RouterProvider router={router} />
 );
 
 export default App;

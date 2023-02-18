015872ef4f53bfdc8c6ce72b8be2eef703f6b8d9
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-02-18 18:34:21 +0100

    Create first page

---
+++ src/history/History.tsx
@@ -0,0 +1,15 @@
+export const History = (): JSX.Element => (
+  <header className="App-header">
+    <p>
+      History.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+);
